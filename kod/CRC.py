321c680a6f75088ee1e40290c03f2bfc6b1536dd
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-04-09 17:17:53 +0200

    Co-authored-by: XDesu <XDesu@users.noreply.github.com>

--- kod/CRC.py
+++ kod/CRC.py
@@ -1,7 +1,14 @@
-def calculate(data):
-    result = 0
-    
-
-    for i in range(len(data))
-
-    return result
+def calc_crc(data : bytes):
+    # funkcja generująca nagłówek CRC
+    # zmiana na tablicę bajtową
+    data = bytearray(data)
+    crc = 0x0000
+    for i in range(0, len(data)):
+        # 
+        crc ^= data[i] << 8
+        for j in range(0, 8):
+            if (crc & 0x8000) > 0:
+                crc =(crc << 1) ^ 0x1021
+            else:
+                crc = crc << 1
+    return crc & 0xFFFF
--- kod/loader.py
+++ kod/loader.py
@@ -1,15 +1,27 @@
 class FileLoader:
     
-    NULL = 0x00
-    
+    current = b''
+    EOF = False
+
     def __init__(self, path):
-        #otorzenie pliku do odczytu(r) binarnego(b)
+        # otorzenie pliku do odczytu(r) binarnego(b)
         self.file = open(path, "rb")
 
 
     def readNext(self, amount=128):
-        #dzielenie na bloki o rozmiarze 128B 
-        data = self.file.read(amount)
-        while len(data) < 128:
-            data += self.NULL
-        return data
+        # dzielenie na bloki o rozmiarze 128B 
+        self.current = self.file.read(amount)
+        # uzupełnienie do 128
+        while len(self.current) < 128:
+            # uzupełnianie pustym znakiem null
+            self.current += b'\x00' * 8 
+            # ustawienie flagi końca pliku
+            self.EOF = True
+        return self.current
+
+    def repeat(self):
+        # powtarzanie ostatniego przesłanego bloku
+        return self.current
+
+    def isEOF(self):
+        return self.EOF
